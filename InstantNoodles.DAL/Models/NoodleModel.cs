9a3906fb1b4ecd257188db1a6163635db3039b4e
Philemon Philippin <p_philippin@hotmail.com>
2022-08-17 07:40:20 +0200

    installed 3 NuGet

---
+++ InstantNoodles.DAL/Models/NoodleModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstantNoodles.DAL.Models;
+
+internal class NoodleModel
+{
+    public int NoodleID { get; set; }
+    public string Name { get; set; }
+    public string Meat { get; set; }
+    public string Vegetable { get; set; }
+    public bool Sauce { get; set; }
+
+}
