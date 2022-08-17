cc1cd4923dc8aabd215610dcfde111e879450d78
Philemon Philippin <p_philippin@hotmail.com>
2022-08-17 18:18:48 +0200

    Auto mapper functional

--- InstantNoodles.MVC/Controllers/NoodleController.cs
+++ InstantNoodles.MVC/Controllers/NoodleController.cs
@@ -1,6 +1,8 @@
-﻿using InstantNoodles.DAL.Data;
-using InstantNoodles.DAL.Models;
+﻿using AutoMapper;
+using InstantNoodles.DAL.Data;
 using Microsoft.AspNetCore.Mvc;
+using NoodleDAL = InstantNoodles.DAL.Models.NoodleModel;
+using NoodleMVC = InstantNoodles.MVC.Models.NoodleModel;
 
 namespace InstantNoodles.MVC.Controllers;
 public class NoodleController : Controller
@@ -12,7 +14,16 @@ public class NoodleController : Controller
     }
     public async Task<IActionResult> Index()
     {
-        IEnumerable<NoodleModel> noodles = await _service.GetNoodles();
+        MapperConfiguration config = new MapperConfiguration(cfg => cfg.CreateMap<NoodleDAL, NoodleMVC>());
+        Mapper mapper = new Mapper(config);
+
+        IEnumerable<NoodleDAL> noodlesDAL = await _service.GetNoodles();
+        List<NoodleMVC> noodles = new List<NoodleMVC>();
+        foreach (NoodleDAL noodleDAL in noodlesDAL)
+        {
+            NoodleMVC noodle = mapper.Map<NoodleMVC>(noodleDAL);
+            noodles.Add(noodle);
+        }
         return View(noodles);
     }
 }
